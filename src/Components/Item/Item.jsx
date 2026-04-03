abd215a0a2a07a8d6c0966068bd6ae923625a171
cuongbam123 <hoquoccuong123socola@gmail.com>
2026-04-03 11:08:41 +0700

    add

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./Item.css";
 import { Link } from "react-router-dom";
 // Nhớ import file helper vào đây (đường dẫn lùi ra ngoài cho đúng thư mục utils)
-import { formatImageUrl } from "../../utils/formatImage"; 
+import { formatImageUrl } from "../../utils/formatImage";
 
 const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        {/* ✅ Bọc props.image qua hàm formatImageUrl */}
-        <img onClick={() => window.scrollTo(0, 0)} src={formatImageUrl(props.image)} alt={props.name} />
+        {/* BẮT BUỘC phải bọc props.image ở đây */}
+        <img
+          onClick={() => window.scrollTo(0, 0)}
+          src={formatImageUrl(props.image)}
+          alt={props.name}
+        />
       </Link>
       <p>{props.name}</p>
       <div className="item-prices">
